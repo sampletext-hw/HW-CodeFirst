ef721a542d3607815b4e2883367cdd9d087302d9
Bird Egop <sampletext32@bk.ru>
2020-11-12 21:24:23 +0300

    Add dependancies and Implement UniversityContext

---
+++ HW-CodeFirst/Data/UniversityContext.cs
@@ -0,0 +1,39 @@
+﻿using HW_CodeFirst.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HW_CodeFirst.Data
+{
+    public class UniversityContext : DbContext
+    {
+        public UniversityContext()
+        {
+        }
+
+        public UniversityContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Resource> Resources { get; set; }
+        public DbSet<Homework> HomeworkSubmissions { get; set; }
+        public DbSet<StudentCourse> StudentCourses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(
+                    @"Server=(localdb)\MSSQLLocalDB;Database=University;Integrated Security = true;Trusted_Connection=True;");
+            }
+
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<StudentCourse>(e =>
+                e.HasKey(sc => new {sc.CourseId, sc.StudentId}));
+        }
+    }
+}
