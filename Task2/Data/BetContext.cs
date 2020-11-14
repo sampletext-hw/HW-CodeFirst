385794fdb7eaff6fdcbaf5b4dcd2f9a41ffd15dc
Bird Egop <sampletext32@bk.ru>
2020-11-15 02:29:25 +0300

    Add BetContext

---
+++ Task2/Data/BetContext.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Task2.Data.Models;
+
+namespace Task2.Data
+{
+    public class BetContext : DbContext
+    {
+        public BetContext()
+        {
+        }
+
+        public BetContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Color> Colors { get; set; }
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<Town> Towns { get; set; }
+        public DbSet<Position> Positions { get; set; }
+        public DbSet<Player> Players { get; set; }
+        public DbSet<Team> Teams { get; set; }
+        public DbSet<Game> Games { get; set; }
+        public DbSet<PlayerStatistic> PlayerStatistics { get; set; }
+        public DbSet<Bet> Bets { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(
+                    @"Server=(localdb)\MSSQLLocalDB;Database=Bets;Integrated Security = true;Trusted_Connection=True;");
+            }
+
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<PlayerStatistic>(e =>
+                e.HasKey(ps => new {ps.PlayerId, ps.GameId}));
+        }
+    }
+}
